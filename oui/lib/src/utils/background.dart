110bf1186a6ae3ae28997c96a69578791509de8c
Luca Silverentand <luca@onezero.company>
2024-12-19 01:25:28 +0100

    Refactor Background widget constructors to use private constructor for improved encapsulation

--- oui/lib/src/utils/background.dart
+++ oui/lib/src/utils/background.dart
@@ -5,24 +5,23 @@ class Background extends StatelessWidget {
   final Gradient? gradient;
   final Widget Function(BuildContext)? custom;
 
-  const Background({
-    super.key,
+  const Background._({
     this.color,
     this.gradient,
     this.custom,
   });
 
-  factory Background.color(Color color) => Background(color: color);
+  factory Background.color(Color color) => Background._(color: color);
 
   factory Background.gradient(Gradient gradient) =>
-      Background(gradient: gradient);
+      Background._(gradient: gradient);
 
   factory Background.custom(
     Widget Function(BuildContext) custom, {
     Color? color,
     Gradient? gradient,
   }) =>
-      Background(
+      Background._(
         color: color,
         gradient: gradient,
         custom: custom,
